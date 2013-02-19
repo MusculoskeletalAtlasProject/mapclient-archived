003ca3ac19f9f2b6b89bc0d8a0e1e943bd4b5796
Hugh Sorby <h.sorby@auckland.ac.nz>
2013-02-20 11:13:08 +1300

    Replacing unittest.main() for unittest.TextTestRunner.run(suite()) in tests/alltests.py.

--- tests/alltests.py
+++ tests/alltests.py
@@ -45,5 +45,5 @@ def load_tests(loader, tests, pattern):
 
 
 if __name__ == '__main__':
-    unittest.main()
-    #unittest.TextTestRunner().run(tests)
+    #unittest.main()
+    unittest.TextTestRunner().run(suite())
