804181bf6ca722dfeaf0dd6da371b3d21be7355f
Hugh Sorby <h.sorby@auckland.ac.nz>
2014-01-28 15:33:32 +1300

    Modifying the plugin manager load test to deal with the introduction of the logger.

--- tests/core/test_pluginframework.py
+++ tests/core/test_pluginframework.py
@@ -17,25 +17,40 @@ This file is part of MAP Client. (http://launchpad.net/mapclient)
     You should have received a copy of the GNU General Public License
     along with MAP Client.  If not, see <http://www.gnu.org/licenses/>..
 '''
-import sys, unittest
+import unittest
+import logging
+
 from mapclient.core.mainapplication import PluginManager
 from tests.utils import ConsumeOutput
+
+def initialiseLogger():
+    logging.basicConfig(format='%(asctime)s %(levelname)s - %(name)s--> %(message)s', datefmt='%Y/%m/%d %H:%M:%S', level='DEBUG')
+    logging.addLevelName(29, 'PLUGIN')
 
 class PluginFrameworkTestCase(unittest.TestCase):
 
 
     def testLoadPlugins(self):
-        pm = PluginManager()
 
-        old_stdout = sys.stdout
-        sys.stdout = redirectstdout = ConsumeOutput()
+        initialiseLogger()
+
+        redirectstdout = ConsumeOutput()
+        root = logging.getLogger('mapclient.core.mainapplication')
+        ch = logging.StreamHandler(redirectstdout)
+        ch.setLevel(29)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        ch.setFormatter(formatter)
+        root.addHandler(ch)
+        root.propagate = False
+
+        pm = PluginManager()
 
         pm.load()
 
-        sys.stdout = old_stdout
-        self.assertTrue("Plugin 'pointcloudserializerstep' version 0.3.0 by Hugh Sorby loaded" in redirectstdout.messages)
+        features = [True for msg in redirectstdout.messages if "Plugin 'pointcloudserializerstep' version 0.3.0 by Hugh Sorby loaded" in msg]
+        self.assertEqual(1, len(features))
 
 
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testLoadPlugins']
+    # import sys;sys.argv = ['', 'Test.testLoadPlugins']
     unittest.main()
