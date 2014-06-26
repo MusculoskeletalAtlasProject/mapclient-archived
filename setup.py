34c5bd39f1e563824f97a40c21e3c635e1d99a40
Hugh Sorby <h.sorby@auckland.ac.nz>
2014-06-26 12:26:57 +1200

    Moved plugins/mapclientplugins to mapclientplugins.  This works better with setup.py.  Also trying to set the required PySide version to the current PySide available on the system to stop setuptools from trying to upgrade  it when it is already present.

--- setup.py
+++ setup.py
@@ -2,21 +2,33 @@
 
 from setuptools import setup, find_packages
 
+try:
+    import PySide
+    pyside_version = PySide.__version__
+    pyside_requirement = 'PySide==' + pyside_version
+except:
+    pyside_requirement = 'PySide'
+install_requires = [
+        pyside_requirement,
+        'requests-oauthlib',
+        'pmr.wfctrl']
+
+print find_packages(exclude=['tests', 'tests.*', ])
+print install_requires
 
 setup(name='mapclient',
-     version='0.11.0',
+     version='0.11.1',
      description='A framework for managing and sharing workflows.',
      author='MAP Client Developers',
      author_email='mapclient-devs@physiomeproject.org',
      url='https://launchpad.net/mapclient',
      namespace_packages=['mapclient', ],
-     packages=find_packages(exclude=['tests', 'tests.*', ]),
+     packages=find_packages(exclude=['tests', 'tests.*', ]) + ['plugins.mapclientplugins'],
      package_data={'mapclient.tools.annotation': ['annotation.voc']},
-     #py_modules=['mapclient.mapclient'],
+     # py_modules=['mapclient.mapclient'],
      entry_points={'console_scripts': ['mapclient=mapclient.application:winmain']},
      install_requires=[
-        'PySide',
+        pyside_requirement,
         'requests-oauthlib',
-        'pmr.wfctrl',
-    ],
+        'pmr.wfctrl']
 )
