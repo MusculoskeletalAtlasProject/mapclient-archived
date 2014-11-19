4ccf1ea2d2280e9517fd1ac561ee0f49a7c337e0
jonathanritchie <jrit927@aucklanduni.ac.nz>
2014-11-19 14:29:11 +1300

    Updated names of objects in UI.

--- mapclient/widgets/ui_loginformation.py
+++ mapclient/widgets/ui_loginformation.py
@@ -2,30 +2,30 @@
 
 # Form implementation generated from reading ui file 'qt/loginformation.ui'
 #
-# Created: Wed Nov 19 13:55:18 2014
+# Created: Wed Nov 19 14:28:27 2014
 #      by: pyside-uic 0.2.15 running on PySide 1.2.2
 #
 # WARNING! All changes made in this file will be lost!
 
 from PySide import QtCore, QtGui
 
-class Ui_Dialog(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(729, 256)
-        self.verticalLayout = QtGui.QVBoxLayout(Dialog)
+class Ui_LogInformation(object):
+    def setupUi(self, LogInformation):
+        LogInformation.setObjectName("LogInformation")
+        LogInformation.resize(729, 256)
+        self.verticalLayout = QtGui.QVBoxLayout(LogInformation)
         self.verticalLayout.setObjectName("verticalLayout")
         self.gridLayout = QtGui.QGridLayout()
         self.gridLayout.setObjectName("gridLayout")
-        self.label = QtGui.QLabel(Dialog)
-        self.label.setObjectName("label")
-        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
-        self.error_table = QtGui.QTableWidget(Dialog)
+        self.history = QtGui.QLabel(LogInformation)
+        self.history.setObjectName("history")
+        self.gridLayout.addWidget(self.history, 0, 0, 1, 1)
+        self.error_table = QtGui.QTableWidget(LogInformation)
         self.error_table.setObjectName("error_table")
         self.error_table.setColumnCount(0)
         self.error_table.setRowCount(0)
         self.gridLayout.addWidget(self.error_table, 1, 0, 1, 1)
-        self.verticalScrollBar = QtGui.QScrollBar(Dialog)
+        self.verticalScrollBar = QtGui.QScrollBar(LogInformation)
         self.verticalScrollBar.setOrientation(QtCore.Qt.Vertical)
         self.verticalScrollBar.setObjectName("verticalScrollBar")
         self.gridLayout.addWidget(self.verticalScrollBar, 1, 1, 1, 1)
@@ -34,18 +34,18 @@ class Ui_Dialog(object):
         self.horizontalLayout.setObjectName("horizontalLayout")
         spacerItem = QtGui.QSpacerItem(598, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
         self.horizontalLayout.addItem(spacerItem)
-        self.pushButton = QtGui.QPushButton(Dialog)
-        self.pushButton.setObjectName("pushButton")
-        self.horizontalLayout.addWidget(self.pushButton)
+        self.exitWindowButton = QtGui.QPushButton(LogInformation)
+        self.exitWindowButton.setObjectName("exitWindowButton")
+        self.horizontalLayout.addWidget(self.exitWindowButton)
         spacerItem1 = QtGui.QSpacerItem(18, 20, QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Minimum)
         self.horizontalLayout.addItem(spacerItem1)
         self.verticalLayout.addLayout(self.horizontalLayout)
 
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
+        self.retranslateUi(LogInformation)
+        QtCore.QMetaObject.connectSlotsByName(LogInformation)
 
-    def retranslateUi(self, Dialog):
-        Dialog.setWindowTitle(QtGui.QApplication.translate("Dialog", "Log Information", None, QtGui.QApplication.UnicodeUTF8))
-        self.label.setText(QtGui.QApplication.translate("Dialog", "History:", None, QtGui.QApplication.UnicodeUTF8))
-        self.pushButton.setText(QtGui.QApplication.translate("Dialog", "Exit", None, QtGui.QApplication.UnicodeUTF8))
+    def retranslateUi(self, LogInformation):
+        LogInformation.setWindowTitle(QtGui.QApplication.translate("LogInformation", "Log Information", None, QtGui.QApplication.UnicodeUTF8))
+        self.history.setText(QtGui.QApplication.translate("LogInformation", "History:", None, QtGui.QApplication.UnicodeUTF8))
+        self.exitWindowButton.setText(QtGui.QApplication.translate("LogInformation", "Exit", None, QtGui.QApplication.UnicodeUTF8))
 
